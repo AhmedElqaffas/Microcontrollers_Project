1d8a55375d27869cd7b26d82eaba3403eea43059
cage97 <44551190+cage97@users.noreply.github.com>
2019-10-31 13:55:32 +0200

    Header file associated with LED.c

---
+++ LED.h
@@ -0,0 +1,11 @@
+#include "tm4c123gh6pm.h"
+#include "DIO_interface.h"
+
+#define PORTF     (1 << 5)
+#define RED       (1 << 1) // pin 1 in port F lights the LED red
+#define GREEN     (1 << 3) // pin 3 in port F lights the LED GREEN
+
+// Prototypes for functions in LED.c
+void LED_Init();
+void activateAlarm();
+void openDoor();
