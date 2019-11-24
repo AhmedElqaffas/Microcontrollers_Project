1c4cb55e1b88d582386dc647fda46135516a30b1
cage97 <44551190+cage97@users.noreply.github.com>
2019-11-24 20:15:40 +0200

    Header file associated with Transitions.c

--- Transitions.h
+++ Transitions.h
@@ -1,5 +1,7 @@
 #include "tm4c123gh6pm.h"
-#include "DIO_interface.h"
+#include "BIT_CALC.h"
+#include "EEPROM.h"
+#include "LCD_Driver.h"
 
 #define MAX_PASS_LENGTH 15 // Maimum number of characters in the password 
 #define CLOSED 0
@@ -7,3 +9,4 @@
 #define ALARM 2
 
 int transit(char pressed);
+
